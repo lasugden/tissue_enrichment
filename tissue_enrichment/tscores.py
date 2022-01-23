6f3de3b0370056be0b09528951fffd787749039a
Arthur Sugden <arthur.sugden@gmail.com>
2022-01-23 12:29:31 -0500

    Added in search from index, added in more statistical functionality, in the midst of dealing with additional randomizations.

--- tissue_enrichment/tscores.py
+++ tissue_enrichment/tscores.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import numpy as np
 import pandas as pd
-from typing import Iterable
+from typing import Iterable, Union
 
 
 def load(*args, **kwargs) -> TissueScores:
@@ -23,26 +23,35 @@ class TissueScores():
     def __init__(self, 
                  path: str = 'data/gene_median_tpm.csv', 
                  combine_skin: bool = True,
-                 alias: bool = True):
+                 alias: bool = True,
+                 alias_path: str = 'data/idx.csv',
+                 info_path: str = 'data/locus_group.csv'):
         """Load in tissue scores/pre-computed edge weights
 
         Args:
             path (str, optional): Path to existing tissue scores. Defaults to 'data/gene_median_tpm.csv'.
             combine_skin (bool, optional): If True, combine two skin tissues as identified in pre-analysis
             alias (bool, optional): If True, load aliases. Defaults to True.
+            alias_path (str, optional): Location of the preprocessed Hugo alias dataframe
+            info_path (str, optional): Location of the preprocessed Hugo locus group dataframe
 
         """
-        self.df = pd.read_csv(path)
+        self.df = pd.read_csv(path).rename(columns={'name': 'ensembl_gene_id', 'description': 'gtex_alias'})
+        self.info = None
+        self._last_gw = None
 
         if combine_skin:
             self.df['skin'] = self.df[['skin.sun_exposed', 'skin.not_sun_exposed']].mean(axis=1)
             self.df = self.df.drop(columns=['skin.sun_exposed', 'skin.not_sun_exposed'])
 
         if alias:
-            pass
+            self.aliases = pd.read_csv(alias_path)
+            self.df = self.df.merge(pd.read_csv(info_path), how='left', on='ensembl_gene_id')
+        else:
+            self.aliases = (self.df[['gtex_alias', 'ensembl_gene_id']]
+                                .rename(columns={'gtex_alias': 'alias'}))
+            self.aliases['alias'] = self.aliases['alias'].str.lower()
 
-        self.df['description'] = self.df['description'].str.lower()
-        self.gene_list = self.df['description'].values
         self.numeric_columns = self.df.select_dtypes(exclude='object').columns.drop('tot_tpm')
 
     def search(self, genes: list[str], raise_error: bool = False) -> tuple[list[str], list[str]]:
@@ -56,16 +65,52 @@ class TissueScores():
             tuple[list[str], list[str]]: tuple of list of found gene names and unfound gene names
         
         """
-        found, unfound = [], []
-        for gene in genes:
-            if gene.lower() in self.gene_list:
-                found.append(gene.lower())
-            else:
-                if raise_error:
-                    raise ValueError(f'Gene name {gene} not found')
-                unfound.append(gene)
+        genes = [v.lower() for v in genes]
+        found = self.aliases[self.aliases['alias'].isin(genes)]['alias'].to_list()
 
+        if raise_error and len(found) < len(genes):
+            missing = list(set(genes).difference(set(found)))
+            raise ValueError(f'Gene name(s) {missing} not found')
+
+        unfound = list(set(genes).difference(set(found)))
         return found, unfound
+
+    def search_df(self, genes: pd.DataFrame, raise_error: bool = False) -> pd.DataFrame:
+        """Search for a list of gene names and return a dataframe for merging
+
+        Args:
+            genes (pd.DataFrame): column of gene names with 'gene' as merge key. Can include other columns.
+            raise_error (bool, optional): If True, raise an error if at least one gene is not found. Defaults to False.
+
+        Returns:
+            pd.DataFrame: merged dataframe of found ensembl_gene_ids
+        
+        """
+        genes['gene'] = genes['gene'].str.lower()
+        found = self.aliases.merge(genes, how='inner', left_on='alias', right_on='gene')
+
+        if raise_error and len(found) < len(genes):
+            missing = set(genes['gene'].to_list()).difference(set(found['alias'].to_list()))
+            raise ValueError(f'Gene name(s) {missing} not found')
+
+        self._last_gw = found
+        return found
+
+    @staticmethod
+    def _format_genes(genes: Union[list[str], list[tuple[str, float]]]) -> pd.DataFrame:
+        """Convert a list of genes or a list of genes and weights to a dataframe for merging
+
+        Args:
+            genes (Union[list[str], list[tuple[str, float]]]): list of genes or list of tuples of genes and weights
+
+        Returns:
+            pd.DataFrame: output dataframe with 2 columns of gene and weight
+        
+        """
+        if not isinstance(genes[0], tuple):
+            genes = [[v, 1.0] for v in genes]
+            
+        return pd.DataFrame(genes, columns=['gene', 'weight'])
     
     def genes(self, genes: list[str], raise_error: bool = False) -> pd.DataFrame:
         """Return a dataframe containing data only for a list of genes
@@ -78,9 +123,27 @@ class TissueScores():
             pd.DataFrame: a dataframe containing only those genes
         
         """
-        genes, _ = self.search(genes, raise_error=raise_error)
-        assert len(genes) > 0, 'No genes found'
-        return self.df.loc[self.df['description'].isin(genes), :].copy()
+        gene_df = self.search_df(self._format_genes(genes), raise_error=raise_error)
+        assert len(gene_df) > 0, 'No genes found'
+        return self.df.merge(gene_df, how='inner', on='ensembl_gene_id')
+
+    def _gene_weights_df(self, gene_df: pd.DataFrame):
+        """Run gene weights on gene_df, for use in fast iteration.
+        Skips lookup.
+
+        Args:
+            gene_weights (pd.DataFrame): dataframe of columns gene, weights
+        
+        Returns:
+            pd.DataFrame: a dataframe containing only those genes, scaled by weights
+
+        """
+        subdf = self.df.merge(gene_df, how='inner', on='ensembl_gene_id')
+        subdf.loc[:, self.numeric_columns] = subdf[self.numeric_columns].multiply(subdf['weight'], axis=0)
+        subdf = subdf[self.numeric_columns].sum(axis=0)
+        subdf = subdf.sort_values(ascending=False)
+
+        return subdf
 
     def gene_weights(self, gene_weights: list[tuple[str, float]], raise_error: bool = False) -> pd.DataFrame:
         """Return a dataframe containing data only for a list of genes scaled by weights
@@ -93,20 +156,63 @@ class TissueScores():
             pd.DataFrame: a dataframe containing only those genes, scaled by weights
         
         """
-        genes, _ = self.search([gw[0].lower() for gw in gene_weights], raise_error=raise_error)
-        gene_weights = list(map({gw[0].lower(): gw for gw in gene_weights}.get, genes))
+        gene_df = self.search_df(self._format_genes(gene_weights), raise_error=raise_error)
 
-        assert len(genes) > 0, 'No genes found'
-        assert max(gene_weights) <= 1, 'Gene weights greater than zero'
-        assert min(gene_weights) >= 0, 'Gene weights less than zero'
+        assert len(gene_df) > 0, 'No genes found'
+        assert gene_df['weight'].max() <= 1, 'Gene weights greater than zero'
+        assert gene_df['weight'].min() >= 0, 'Gene weights less than zero'
 
-        subdf = self.df.loc[self.df['description'].isin(genes), :].copy()
-        subdf.loc[:, self.numeric_columns] = subdf[self.numeric_columns].multiply(
-            subdf['description'].map({gw[0].lower(): gw[1] for gw in gene_weights}), axis=0)
-        subdf = subdf[self.numeric_columns].sum(axis=0)
-        subdf = subdf.sort_values(ascending=False)
+        return self._gene_weights_df(gene_df)
 
-        return subdf
+    def gene_weights_p(self,
+                       gene_weights: list[tuple[str, float]], 
+                       samples: int = 1_000,
+                       within_group: bool = True,
+                       raise_error: bool = False) -> pd.DataFrame:
+        """Search with genes and weights a list of tissues and scores
+
+        Args:
+            gene_weights (list[tuple[str, float]]): list of tuples of gene names and weights
+            samples (int, optional): number of random samples to compare to. Defaults to 1000.
+            within_group (bool, optional): if True, randomize only within locus_group as defined by hugo
+            raise_error (bool, optional): If True, raise an error if at least one gene is not found. Defaults to False.
+
+        Returns:
+            pd.DataFrame: return a dataframe of scores and pvalues
+        
+        """
+        real = self.gene_weights(gene_weights, raise_error=raise_error).rename('score').to_frame()
+        gene_weights_loci = self._last_gw.merge(self.df, how='left', on='ensembl_gene_id')
+        locus_groups = None if not within_group else gene_weights_loci['locus_group'].to_list()
+        rand = self.random_gene_weights(gene_weights_loci['weight'].to_list(), 
+                                        locus_groups=locus_groups, 
+                                        samples=samples)
+        real['pvalue'] = real['score'].apply(lambda x: (rand > x).sum()/samples)
+        return real
+
+    def optimal_tissue(self,
+                       gene_weights: list[tuple[str, float]],
+                       samples: int = 1_0000,
+                       within_group: bool = True,
+                       raise_error: bool = False):
+
+        real = self.gene_weights(gene_weights, raise_error=raise_error).rename('score').to_frame()
+        real['inv_rank'] = 1.0/(np.arange(len(real)) + 1)
+        tissue_scores = [real['score'].iloc[0]]
+
+        gw = self._last_gw
+        while len(gw) > 1:
+            subdf = gw.merge(self.df, how='left', on='ensembl_gene_id')
+            subdf.loc[:, self.numeric_columns] = subdf[self.numeric_columns].multiply(subdf['weight'], axis=0)*real['inv_rank']
+            subdf['gene_tissue_score'] = subdf[self.numeric_columns].sum(axis=1)
+            
+            gw = gw.drop(subdf['gene_tissue_score'].idxmin()).reset_index(drop=True)
+            real = self._gene_weights_df(gw).rename('score').to_frame()
+            real['inv_rank'] = 1.0/(np.arange(len(real)) + 1)
+            tissue_scores.append(real['score'].iloc[0])
+
+        subdf = self.df.merge(gene_df, how='inner', on='ensembl_gene_id')
+        print(1)
 
     def feature_matrix(self) -> np.ndarray:
         """Return a feature matrix
@@ -128,6 +234,7 @@ class TissueScores():
 
     def random_genes(self, 
                      n: int, 
+                     locus_groups: Iterable[str] = None,
                      samples: int = 1_000, 
                      keep_tissues: bool = False) -> np.ndarray:
         """Create a random sample of weights
@@ -135,6 +242,8 @@ class TissueScores():
         Args:
             n (int): number of genes to include
             samples (int, optional): number of random samples to compute. Defaults to 1000.
+            locus_groups (str, optional): locus group to randomize within. If None,
+                randomize within all locus_groups
             keep_tissues (bool, optional): If True, return results for all tissues. Defaults to False.
 
         Returns:
@@ -142,16 +251,19 @@ class TissueScores():
         
         """
         weights = [1]*n
-        return self.random_gene_weights(weights, samples, keep_tissues)
+        return self.random_gene_weights(weights, locus_groups, samples, keep_tissues)
 
     def random_gene_weights(self, 
                             weights: Iterable[float], 
+                            locus_groups: Iterable[str] = None,
                             samples: int = 1_000,
                             keep_tissues: bool = False) -> np.ndarray:
         """Create a random sample of weights
 
         Args:
             weights (Iterable[float]): weights of genes to apply
+            locus_group (str, optional): locus group to randomize within. If None,
+                randomize within all locus_groups
             samples (int): number of random samples to compute
             keep_tissues (bool, optional): If True, return results for all tissues. Defaults to False.
 
@@ -159,10 +271,36 @@ class TissueScores():
             np.ndarray: array of top scores as floats
         
         """
-        rdf = self.df.sample(n=len(weights)*samples, replace=True)[self.numeric_columns].values
+        if locus_groups is None:
+            rdf = self.df.sample(n=len(weights)*samples, replace=True)[self.numeric_columns].values
+        else:
+            lgs = pd.DataFrame({'weights': weights, 'lg': locus_groups})
+            lgs = lgs.groupby('lg').agg(list)
+            
+            rdfs = []
+            for lg, weights in lgs['weights'].iteritems():
+                rdfs.append(self.df
+                                .loc[self.df['locus_group'] == lg, :]
+                                .sample(n=len(weights)*samples, replace=True)[self.numeric_columns]
+                                .values)
+            rdf = np.concatenate(rdfs, axis=0)
+        
         rdf = rdf.transpose()*np.repeat(weights, samples)
         tops = rdf.reshape((rdf.shape[0], len(weights), -1)).sum(axis=1)
         if keep_tissues:
             return tops
         else:
             return tops.max(axis=0)
+
+
+if __name__ == '__main__':
+    ts = load()
+    ts.optimal_tissue([('hla-a', 0.5), 
+                          ('hla-b', 0.6),
+                          ('hla-c', 1), 
+                          ('hla-dpa1', 1), 
+                          ('hla-dpb1', 1), 
+                          ('hla-dqa1', 1),
+                          ('hla-dqb1', 1), 
+                          ('hla-dra', 1), 
+                          ('hla-drb1', 1)])
